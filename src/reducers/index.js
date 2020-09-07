5afaff5455432c9a95961195f31efcfb79837224
wlad999 <wlad25888@gmail.com>
2020-09-07 14:18:13 +0300

    145 new Components connected to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,21 @@
 const initialState = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [{
+        id: 1,
+        name: "Book 1",
+        count: 3,
+        total: 150
+    },
+        {
+            id: 2,
+            name: "Book 2",
+            count: 2,
+            total: 70
+        }],
+    orderTotal: 220,
+    // total: null
 }
 
 
@@ -9,18 +23,21 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "FETCH_BOOKS_REQUEST":
             return {
+                ...state,
                 books: [],
                 loading: true,
                 error: null
             }
         case "FETCH_BOOKS_SUCCESS":
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
             };
         case "FETCH_BOOKS_FAILURE" :
             return {
+                ...state,
                 books: [],
                 loading: false,
                 error: action.payload
