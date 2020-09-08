baa07c63712f5ca026bd368dc947e66c5cc47725
wlad999 <wlad25888@gmail.com>
2020-09-08 13:29:24 +0300

    147 update array item in redux - redux-reduce

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,28 @@ const initialState = {
     cartItems: [],
     orderTotal: 220,
 }
+const updateCartItems = (cartItems, newItem, idx) => {
+    if (idx === -1) {
+        return [...cartItems, newItem]
+    }
+    return cartItems.map(item => item.id === newItem.id ? newItem : item)
+}
+
+const updateCartItem = (book, item = {}) => {
+    const {
+        id = book.id,
+        count = 0,
+        title = book.title,
+        total = 0
+    } = item
+    return {
+        id,
+        title,
+        count: count + 1,
+        total: total + book.price
+    }
+
+}
 
 
 const reducer = (state = initialState, action) => {
@@ -33,17 +55,30 @@ const reducer = (state = initialState, action) => {
         case "BOOK_ADDED_TO_CART":
             const bookId = action.payload
             const book = state.books.find((book) => book.id === bookId)
-            const newItem = {
-                id: book.id,
-                name: book.title,
-                count: 1,
-                total: book.price
-            }
-
+            const itemIndex = state.cartItems.findIndex(({id}) => id === book.id)
+            const item = state.cartItems[itemIndex]
+            // let newItem
+            // if (item) {
+            //     newItem = {
+            //         ...item,
+            //         count: item.count + 1,
+            //         total: item.total + book.price
+            //     }
+            // } else {
+            //     newItem = {
+            //         id: book.id,
+            //         title: book.title,
+            //         count: 1,
+            //         total: book.price
+            //     }
+            // }
+            const newItem = updateCartItem(book, item)
             return {
                 ...state,
-                cartItems: [...state.cartItems, newItem]
+                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
             }
+
+
         default:
             return state
     }
