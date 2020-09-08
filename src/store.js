4806670fd228acba1b46ef06c02a899628c19bfd
wlad999 <wlad25888@gmail.com>
2020-09-08 21:00:07 +0300

    150 store enhancer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,30 @@
-import {createStore} from "redux";
+import {createStore, compose} from "redux";
 import reducer from "./reducers";
 
-const store = createStore(reducer)
+const logEnhancer = (createStore) => (...agrs) => {
+    const store = createStore(...agrs)
+    const originalDispatch = store.dispatch
+    store.dispatch = (action) => {
+        console.log("action.type", action.type)
+        originalDispatch(action)
+    }
+    return store
+}
+
+const stringEnhancer = (createStore) => (...agrs) => {
+    const store = createStore(...agrs)
+    const originalDispatch = store.dispatch
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            })
+        }
+        originalDispatch(action)
+    }
+    return store
+}
+
+
+const store = createStore(reducer, compose(stringEnhancer, logEnhancer))
 export default store
